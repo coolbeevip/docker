84471719f2c536c91a86508d9cba121c7c5b40c1
Lei Zhang <zhanglei@apache.org>
2022-09-15 14:50:59 +0800

    add jacorb name server

---
+++ jacorb-name-server/README.md
@@ -0,0 +1,4 @@
+
+```
+docker run --rm -it -p 12000:12000 coolbeevip/jacorb-name-server:3.9
+```
---
+++ jacorb-name-server/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+java -Djava.endorsed.dirs=/jacorb-3.9/lib \
+-Djacorb.home=/jacorb-3.9 \
+-DOAIAddr=0.0.0.0 \
+-DOAPort=12000 \
+-Djacorb.dns.enable=on \
+-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB \
+-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton \
+-Djacorb.naming.purge=on \
+-Djacorb.naming.ior_filename=ns.ior \
+org.jacorb.naming.NameServer
