b0f0750762eaedb7760f81287370225a24dce437
Lei Zhang <zhanglei@apache.org>
2022-09-16 14:27:35 +0800

    add jacorb name server

--- jacorb-name-server/README.md
+++ jacorb-name-server/README.md
@@ -1,4 +1,34 @@
 
+本机使用
+
 ```
-docker run --rm -it -p 12000:12000 coolbeevip/jacorb-name-server:3.9
+docker run --rm -it \
+--name=jacorb-name-server \
+-p 12000:12000 \
+coolbeevip/jacorb-name-server:3.9
+```
+
+## 使用域名解析
+
+客户端需要在 /etc/hosts 中配置 jacorb.server 指向本镜像的可访问地址
+
+```
+docker run --rm -it \
+-p 12000:12000 \
+-e JACORB_IOR_PROXY_HOST=jacorb.server \
+coolbeevip/jacorb-name-server:3.9
+```
+
+## 使用域名解析 & 端口映射
+
+客户端需要在 /etc/hosts 中配置 jacorb.server 指向本镜像的可访问地址
+客户端通过 22000 访问容器对外映射的 22000 端口
+
+```
+docker run --rm -it \
+-p 22000:12000 \
+-e JACORB_DNS_ENABLE=on \
+-e JACORB_IOR_PROXY_HOST=jacorb.server \
+-e JACORB_IOR_PROXY_PORT=22000 \
+coolbeevip/jacorb-name-server:3.9
 ```
--- jacorb-name-server/start.sh
+++ jacorb-name-server/start.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-java -Djava.endorsed.dirs=/jacorb-3.9/lib \
+PARAM=''
+if [ "$JACORB_IOR_PROXY_HOST" == "" ]; then
+  JACORB_IOR_PROXY_HOST='localhost'
+fi
+if [ "$JACORB_IOR_PROXY_PORT" == "" ]; then
+  JACORB_IOR_PROXY_PORT=12000
+fi
+
+echo 'JACORB_IOR_PROXY_HOST='$JACORB_IOR_PROXY_HOST
+echo 'JACORB_IOR_PROXY_PORT='$JACORB_IOR_PROXY_PORT
+
+java \
+-Djava.endorsed.dirs=/jacorb-3.9/lib \
 -Djacorb.home=/jacorb-3.9 \
 -DOAIAddr=0.0.0.0 \
 -DOAPort=12000 \
--Djacorb.dns.enable=off \
+-Djacorb.dns.enable=on \
+-Djacorb.ior_proxy_host=$JACORB_IOR_PROXY_HOST \
+-Djacorb.ior_proxy_port=$JACORB_IOR_PROXY_PORT \
 -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB \
 -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton \
 -Djacorb.naming.purge=on \
