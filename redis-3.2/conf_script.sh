d576a96d5e920d4df62e9222044913f586337c3e
Lei Zhang <zhanglei@apache.org>
2023-10-21 20:25:12 +0800

    Add redis 3.2

--- redis-3.2/conf_script.sh
+++ redis-3.2/conf_script.sh
@@ -3,10 +3,6 @@
 if [ "$REDIS_PASSWORD" != "" ]; then
   echo "init redis password"
   echo -e "\nrequirepass $REDIS_PASSWORD" >> /redis/redis.conf
-
-  if [ $CLUSTER_ANNOUNCE_IP ]; then
-    echo -e "\nmasterauth $REDIS_PASSWORD" >> /redis/redis.conf
-  fi
 fi
 
 if [ "$REDIS_MAXMEMORY" != "" ]; then
@@ -17,17 +13,4 @@ fi
 if [ "$REDIS_PORT" != "" ]; then
   echo "init redis port"
   sed -i "s/port 6379/port $REDIS_PORT/g" /redis/redis.conf
-fi
-
-if [ $CLUSTER_ANNOUNCE_IP ]; then
-  sed -i "s/cluster-enabled no/cluster-enabled yes/g" /redis/redis.conf
-  sed -i "s/cluster-announce-ip 127.0.0.1/cluster-announce-ip $CLUSTER_ANNOUNCE_IP/g" /redis/redis.conf
-fi
-
-if [ $CLUSTER_ANNOUNCE_PORT ]; then
-    sed -i "s/cluster-announce-port 6379/cluster-announce-port $CLUSTER_ANNOUNCE_PORT/g" /redis/redis.conf
-fi
-
-if [ $CLUSTER_ANNOUNCE_BUS_PORT ]; then
-    sed -i "s/cluster-announce-bus-port 16379/cluster-announce-bus-port $CLUSTER_ANNOUNCE_BUS_PORT/g" /redis/redis.conf
 fi
